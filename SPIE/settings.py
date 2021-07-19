74ceb78a145e69b3dd0a47e55804d75e6aef0f29
Shubham Das <skshubhamdas@gmail.com>
2021-07-19 23:01:17 +0530

    changed settings.py

--- SPIE/settings.py
+++ SPIE/settings.py
@@ -25,9 +25,9 @@ SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 SECRET_KEY = 'django-insecure-04gcb@#x^uev+%c90*5*&vpe+2^$pp70ke=%c53pv(#49g4fo$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
